2acaf69e83c45cbb70d6d39f48729fdbe4150de0
umasankarananth <parumasan12@gmail.com>
2016-09-22 22:01:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function(req, res){
-    var articleName = req.params.articlename;
+    var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
