93e5813400dd207c2f770196e5220a4a4ba072f8
umasankarananth <parumasan12@gmail.com>
2016-09-22 18:51:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,21 +4,84 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles={
+var articleone ={
+    title : 'Article One| Uma ',
+    heading : 'Aricle One',
+    date : 'Sep 5, 2016',
+    Content :     `<p>
+                          This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file.
+                    </p>
+                    <p>
+                           This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file.
+                    </p>
+                    <p>
+                             This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file. This is the content for first file.
+                
+                    </p>`
+};
+ var articletwo = {
+     title : 'Article Two | Uma',
+     heading : '',Article Two
+     date : 'Sep 10, 2016',
+     Content : `
+     <p>
+                      This is the content for Second file. 
+                </p>`
+ };
+ var articlethree = {
+     title : 'Article Three | Uma',
+     heading : '',Article Three
+     date : 'Sep 15, 2016',
+     Content : `
+     <p>
+                      This is the content for Third file. 
+                </p>`
+ };
 
+};
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var Content = data.Content;
+
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+        ${title}
+        </title>
+            <meta name ="viewport" content = "width-device-width, initial-scale-1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class = "container">
+                <div>
+                    <a href= "/">Home</a>
+                </div>
+                <hr>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                   ${Content}
+                </div>
+        </div>        
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
-});
-
-app.get('/article-two', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));  
-});
-
-app.get('/article-three', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
+  res.send(createTemplate(articleone));
 });
 
 app.get('/ui/style.css', function (req, res) {
