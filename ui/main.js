907fbec6ba87e48f4b5408eb2958374b28c80ea7
umasankarananth <parumasan12@gmail.com>
2016-09-24 16:13:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,17 @@ console.log('Loaded!');
 //change the text of main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+// move the image left by 100px on clicking the image .To do that Uncomment made on Line 15 .and comment remaining lines.
+ // To move the image towards Right slowly on clicking the image , use the following syntax and comment line 15 and Uncomment remaining lines. Except lines 8,14,15
 var img = document.getElementById('madi');
+var marginLeft= 0;
+function moveRight(){
+    marginLeft = margin + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+   // img.style.marginLeft = '100px';
+   var interval = setInterval(moveRight , 50);
 };
+  
