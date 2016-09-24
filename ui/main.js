42799fc4a64d015ee3b3719d0ea2d013343f9aa9
umasankarananth <parumasan12@gmail.com>
2016-09-24 16:00:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
-img.OnClick = function() {
-    madi.style.marginLeft = '100px';
+img.onclick = function () {
+    img.style.marginLeft = '100px';
 };
