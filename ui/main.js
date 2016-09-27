39a3fc1c406a2a0d698658c71eae71f7dc5b3216
umasankarananth <parumasan12@gmail.com>
2016-09-27 20:50:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,24 @@ console.log('Loaded!');
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-};
-  
+        //Create a request Object
+        var request = new XMLHttpRequest();
+        // capture the response & stored in a variable
+        request.onreadystatechange =function(){
+            if(request.readyState === XMLHttpRequest.DONE){
+                    // Take some action
+             if(request.status ===200){
+                    var counter = request.responseText;
+                    var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+                }
+        }
+    
+    //counter = counter + 1;
+    //Not done yet
+     };
+
+ // Make the request
+  request.open('GET' ,'http://http://umasankarananth.imad.hasura-app.io/counter',true);
+  request.send(null);
+  };
