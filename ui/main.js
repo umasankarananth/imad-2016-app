f848e3d9fa82350f8d5312972b755e71282392d7
umasankarananth <parumasan12@gmail.com>
2016-09-27 20:53:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,6 +43,6 @@ button.onclick = function(){
      };
 
  // Make the request
-  request.open('GET' ,'http://http://umasankarananth.imad.hasura-app.io/counter',true);
+  request.open('GET' ,'http://umasankarananth.imad.hasura-app.io/counter',true);
   request.send(null);
   };
