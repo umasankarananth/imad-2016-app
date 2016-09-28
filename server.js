b48aaaf7d6430e4a36e155234236e187bb582459
umasankarananth <parumasan12@gmail.com>
2016-09-28 13:38:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ var names = [];
 app.get('/submit-name', function(req,res){
     //get the name from  request
     var name = req.query.name;
-   // names.push(name);
+    names.push(name);
     res.send(JSON.stringify('names'));
 });
 
