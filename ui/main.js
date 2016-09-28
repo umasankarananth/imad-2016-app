8112c9b1d91db50ea2d851be73c66b21c1c016ce
umasankarananth <parumasan12@gmail.com>
2016-09-28 14:46:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,8 +22,8 @@ console.log('Loaded!');
 
 // COUNTER CODE
 
-var button = document.getElementById('counter');
-var counter = 0;
+//var button = document.getElementById('counter');
+//var counter = 0;
 //button.onclick = function(){
         
    //  };
@@ -32,43 +32,43 @@ var counter = 0;
   
   //SUBMIT NAME
   
-   var submit = document.getElementById('submit_btn');
+  var submit = document.getElementById('submit_btn');
   submit.onclick = function(){
-              //Create a request Object
+           //Create a request Object
                 var request = new XMLHttpRequest();
-                // capture the response & stored in a variable
-                request.onreadystatechange = function(){
-                    if(request.readyState === XMLHttpRequest.DONE){
-                            // Take some action
-                     if(request.status === 200){
+           // capture the response & stored in a variable
+                 request.onreadystatechange = function(){
+                 if(request.readyState === XMLHttpRequest.DONE){
+           // Take some action
+                 if(request.status === 200){
                          
-                        //    var counter = request.responseText;
-                        //    var span = document.getElementById('count');
-                        // span.innerHTML = counter.toString();
-                       // }
-                        // }
-            
-                 //counter = counter + 1;
-               
-              //capture the list of names and render it as a list
-             // var names = ['name1', 'name2', 'name3','name4'];
-             var names= request.responseText;
-             names = JSON.parse(names);
-              var list ='';
-              for(var i = 0; i< names.length; i++){
-                  list += '<li>' + names[i] +'</li>';
-              }
-              var ul = document.getElementById('namelist');
-              ul.innerHTML = list;
-                     }
+                    //    var counter = request.responseText;
+                    //    var span = document.getElementById('count');
+                    // span.innerHTML = counter.toString();
+                    // }
+                    // }
+        
+                    //counter = counter + 1;
+             
+                    // var names = ['name1', 'name2', 'name3','name4'];
+          //capture the list of names and render it as a list
+                    var names= request.responseText;
+                    names = JSON.parse(names);
+                    var list ='';
+                    for(var i = 0; i< names.length; i++){
+                    list += '<li>' + names[i] +'</li>';
+                    }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
+                    }
                 }   
                  //Not done yet
-          };
+            };
           //Make request to server and send the name
-           var nameInput = document.getElementById('name');
-           var name = nameInput.value;
-            request.open('GET' ,'http://umasankarananth.imad.hasura-app.io/submit-name?name=' + name, true);
-            request.send(null);
-      };
+                     var nameInput = document.getElementById('name');
+                     var name = nameInput.value;
+                     request.open('GET' ,'http://umasankarananth.imad.hasura-app.io/submit-name?name=' + name, true);
+                     request.send(null);
+    };
      
           
