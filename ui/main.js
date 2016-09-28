241608893a72fbd664f46d01962c24e2060473bb
umasankarananth <parumasan12@gmail.com>
2016-09-28 14:13:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,41 +25,50 @@ console.log('Loaded!');
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
-        //Create a request Object
-        var request = new XMLHttpRequest();
-        // capture the response & stored in a variable
-        request.onreadystatechange =function(){
-            if(request.readyState === XMLHttpRequest.DONE){
-                    // Take some action
-             if(request.status ===200){
-                    var counter = request.responseText;
-                    var span = document.getElementById('count');
-        span.innerHTML = counter.toString();
-                }
-        }
-    
-    //counter = counter + 1;
-    //Not done yet
+        
      };
 
  // Make the request
-  request.open('GET' ,'http://umasankarananth.imad.hasura-app.io/counter',true);
-  request.send(null);
-  };
   
   //SUBMIT NAME
   
-  var nameInput = document.getElementById('name');
-  var name = nameInput.value;
-  var submit = document.getElementById('submit_btn');
+   var submit = document.getElementById('submit_btn');
   submit.onclick = function(){
-      //Make request to server and send the name
-      //capture the list of names and render it as a list
-      var names = ['name1', 'name2', 'name3','name4'];
-      var list ='';
-      for(var i = 0; i< names.length; i++){
-          list += '<li>' + names[i] +'</li>';
-      }
-      var ul = document.getElementById('namelist');
-      ul.innerHTML = list;
-  };
+              //Create a request Object
+                var request = new XMLHttpRequest();
+                // capture the response & stored in a variable
+                request.onreadystatechange = function(){
+                    if(request.readyState === XMLHttpRequest.DONE){
+                            // Take some action
+                     if(request.status ===200){
+                         
+                        //    var counter = request.responseText;
+                        //    var span = document.getElementById('count');
+                        // span.innerHTML = counter.toString();
+                       // }
+               // }
+            
+                 //counter = counter + 1;
+               
+              //capture the list of names and render it as a list
+             // var names = ['name1', 'name2', 'name3','name4'];
+             var names= request.responseText;
+             names = JSON.parse(names);
+              var list ='';
+              for(var i = 0; i< names.length; i++){
+                  list += '<li>' + names[i] +'</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;
+                     }
+                }   
+                 //Not done yet
+          };
+          //Make request to server and send the name
+           var nameInput = document.getElementById('name');
+           var name = nameInput.value;
+            request.open('GET' ,'http://umasankarananth.imad.hasura-app.io/submit-name?name' +name,true);
+            request.send(null);
+      };
+     
+          
