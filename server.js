01ce29ae7cc3c9ea4609ea0e6c9850188000637e
umasankarananth <parumasan12@gmail.com>
2016-10-27 13:15:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var path = require('path');
 var Pool = require('pg').Pool;
 
 var config = {
-      user: umasankarananth,
-      database: umasankarananth,
-      host: db.imad.hasura-app.io,
-      port: 5432,
+      user: 'umasankarananth',
+      database: 'umasankarananth',
+      host: 'db.imad.hasura-app.io',
+      port: '5432',
       password: process.env.DB_PASSWORD
  };
 
