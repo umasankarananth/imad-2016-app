486f3318e8223f4007e143ed19959d21f5626335
umasankarananth <parumasan12@gmail.com>
2016-10-27 14:11:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,9 +119,23 @@ app.get('/submit-name', function(req,res){
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function(req, res){
-    var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function(req, res){
+   // var articleName = req.params.articleName;
+   //SELECT * FROM article where title = 'article-one'
+   pool.query("SELECT * FROM article where title = '" + req.params.articleName + "'", function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+      }else{
+          if(result.rows.length=== 0){
+              res.status(404).send('Article not found');
+          }else{
+              var articleData = result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+      }
+      
+   });
+  
 });
 
 app.get('/ui/style.css', function (req, res) {
