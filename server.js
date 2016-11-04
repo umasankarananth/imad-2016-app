9b2da015f99ff1ba0b975c7d6a49f7919402c69d
umasankarananth <parumasan12@gmail.com>
2016-11-04 17:18:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/submit-name', function(req,res){
 app.get('/articles/:articleName', function(req, res){
    // var articleName = req.params.articleName;
    //SELECT * FROM article where title = '\'; DELETE WHERE a = '\asd'
-   pool.query("SELECT * FROM test WHERE title = $1", [req.params.articleName], function(err,result){
+   pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName +"'", function(err,result){
        if(err){
            res.status(500).send(err.toString());
       }else{
