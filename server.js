8f1834703747238f27a537d969c2439ad595bf65
umasankarananth <parumasan12@gmail.com>
2016-11-07 17:18:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var app = express();
 app.use(morgan('combined'));
 
 
-function createTemplate(data){
+//function createTemplate(data){
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
@@ -51,6 +51,39 @@ var htmlTemplate = `
 `;
 return htmlTemplate;
 }
+function createTemplate(data){
+    var fruitid = data.fruitid;
+    var fruitname = data.fruitname;
+    var fruitrate = data.fruitrate;
+
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+             WELCOME TO FRUITS SUPER MARKET
+        </title>
+            <meta name ="viewport" content = "width-device-width, initial-scale-1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class = "container">
+                <div>
+                    <a href= "/">Home</a>
+                </div>
+                <hr>
+                <h3>
+                    ${fruitname}
+                </h3>
+                <div>
+                   ${rate}
+                </div>
+        </div>        
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
