464e7a80a656cb7889e36e16bf5156d0ff85450e
umasankarananth <parumasan12@gmail.com>
2016-11-09 23:45:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,10 +29,10 @@
         
                     //counter = counter + 1;
              
-                    // var names = ['name1', 'name2', 'name3','name4'];
+                     var names = ['name1', 'name2', 'name3','name4'];
           //capture the list of names and render it as a list
                     //var fruitlist= ['apple','orange','grapes'];
-                    var names = request.responseText;
+                    //var names = request.responseText;
                     names = JSON.parse(names);
                     var list ='';
                     for(var i = 0; i< names.length; i++){
