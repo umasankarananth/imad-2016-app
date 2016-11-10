66180e6c91956ea49f140e64f3463802ef26b7a9
umasankarananth <parumasan12@gmail.com>
2016-11-10 18:04:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ app.get('/product-entry', function(req,res){
               
              // for(var i=0; i < result.rows.length; i++)
               //{
-             var fruitData =result.rows;
+             var fruitData =result.rows[0];
               //res.send(JSON.stringify(fruitData));
               res.send(create(fruitData));
             //  }
