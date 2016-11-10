b765f05c4263bd1ec72ced7b3da5c7e611a2e929
umasankarananth <parumasan12@gmail.com>
2016-11-10 23:55:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,11 +106,12 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('/:fname', function (req, res) {
+app.get('/orange', function (req, res) {
   //res.sendFile(path.join(__dirname, 'ui', 'orange.html'));
   //Make a select request
   //return the response with the results
-    pool.query("SELECT * FROM fruitprice where fruitName ='" +req.params.fname +"'", function(err,result){
+   // pool.query("SELECT * FROM fruitprice where fruitName ='" +req.params.fname +"'", function(err,result){
+      pool.query('SELECT * FROM fruitprice',function(err,result){
        if(err){
           res.status(500).send(err.toString());
       }else{
