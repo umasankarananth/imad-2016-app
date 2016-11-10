fbaa6e8d7e4aaa1a74aab29ed3f73597e6b2ddc3
umasankarananth <parumasan12@gmail.com>
2016-11-10 17:54:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -150,9 +150,10 @@ app.get('/product-entry', function(req,res){
           if(result.rows.length=== 0){
               res.status(404).send('fruit not found');
           }else{
-              for(var i=0; i<result.rows.length;i++)
+              var fruitData;
+              for(var i=0; i < result.rows.length; i++)
               {
-              var fruitData =result.rows[i];
+              fruitData =result.rows[i];
               //res.send(JSON.stringify(fruitData));
               res.send(create(fruitData));
               }
