b42a2c5bada50bed3ce5a693fc284fb64a4589dc
umasankarananth <parumasan12@gmail.com>
2016-11-17 22:19:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/', function (req, res) {
 });
 
 var pool = new Pool(config);
-app.get('/fruits/:fruitName', function (req, res) {
+app.get('/:fruitName', function (req, res) {
     //Make a select request
   //return the response with the results
     pool.query("SELECT * FROM fruitprice where fruitName ='" +req.params.fruitName +"'", function(err,result){
