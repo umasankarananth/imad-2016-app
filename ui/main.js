da8db46a148b0a18b103ced67a36abdbae08a596
umasankarananth <parumasan12@gmail.com>
2016-11-18 09:04:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,6 +30,7 @@
                       console.log(username);
                       console.log(password);
                      request.open('POST' ,'http://umasankarananth.imad.hasura-app.io/login', true);
+                     request.setRequestHeader('Content-Type','application/json');
                      request.send(JSON.stringify({username:username, password: password}));
                           
 
