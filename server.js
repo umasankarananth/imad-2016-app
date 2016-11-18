d4bc90a98cf751863253b79f519461cd56dd4f7c
umasankarananth <parumasan12@gmail.com>
2016-11-18 13:14:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -205,19 +205,6 @@ app.get('/mango', function (req, res) {
 });
   
 
-  
-
-
-var names = [];
-app.get('/submit-name', function(req,res){
-    //get the name from  request
-     var name = req.params.name;
-   //var name= req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
-
-
 app.get('/welcomeform', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'welcomeform.html'));
 });
