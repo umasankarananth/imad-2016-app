064be8eabb8c7ac5e3898b8a8ec3385ebc2cf542
umasankarananth <parumasan12@gmail.com>
2016-11-18 09:01:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,38 +3,27 @@
   
   var submit = document.getElementById('submit_btn');
   
-  sub.onclick = function(){ 
+  submit.onclick = function(){ 
            //Create a request Object
                 var request = new XMLHttpRequest();
            // capture the response & stored in a variable
                  request.onreadystatechange = function(){
                  if(request.readyState === XMLHttpRequest.DONE){
-           // Take some action
-                 if(request.status === 200){
-                         
-                    //    var counter = request.responseText;
-                    //    var span = document.getElementById('count');
-                    // span.innerHTML = counter.toString();
-                    // }
-                    // }
-        
-                    //counter = counter + 1;
-             
-                     //var names = ['name1', 'name2', 'name3','name4'];
-          //capture the list of names and render it as a list
-                    //var fruitlist= ['apple','orange','grapes'];
-                    var names = request.responseText;
-                    names = JSON.parse(names);
-                    var list ='';
-                    for(var i = 0; i< names.length; i++){
-                    list += '<li>' + names[i] +'</li>';
-                    }
-                    var ul = document.getElementById('namelist');
-                    ul.innerHTML = list;
-                    }
-                }   
-                 //Not done yet
-            };
+                     //Take some action
+                     if(request.status === 200){
+                         console.log('User Logged in');
+                         alert('Logged in Successfully');
+                     }
+                     else if(request.status=== 403){
+                         alert('Username/Password is Incorrect');
+                     }
+                     else if(request.status===500){
+                         alert('Something went wrong in the server');
+                     }
+                    
+                }
+                //Not done yet
+         };
           //Make request to server and send the name
                      var username = document.getElementById('username').value;
                       var password = document.getElementById('password').value;
@@ -42,6 +31,7 @@
                       console.log(password);
                      request.open('POST' ,'http://umasankarananth.imad.hasura-app.io/login', true);
                      request.send(JSON.stringify({username:username, password: password}));
-    };
-     
+                          
+
+  };
           
