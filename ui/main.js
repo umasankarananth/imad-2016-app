d10cfd619cca5dc2f4c3e64d354a36dfc5c164b4
umasankarananth <parumasan12@gmail.com>
2016-11-18 08:46:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,9 @@
 
-// COUNTER CODE
-
-//var button = document.getElementById('counter');
-//var counter = 0;
-//button.onclick = function(){
-        
-   //  };
-
- // Make the request
-  
-  //SUBMIT NAME
+  //SUBMIT username password to Login
   
   var sub = document.getElementById('sub_btn');
-  sub.onclick = function(){
+  
+  sub.onclick = function(){ 
            //Create a request Object
                 var request = new XMLHttpRequest();
            // capture the response & stored in a variable
@@ -47,8 +38,8 @@
           //Make request to server and send the name
                      var nameInput = document.getElementById('name');
                      var name = nameInput.value;
-                     request.open('GET' ,'http://umasankarananth.imad.hasura-app.io/submit-name?name' + name, true);
-                     request.send(null);
+                     request.open('POST' ,'http://umasankarananth.imad.hasura-app.io/submit-name?name' + name, true);
+                     request.send(JSON.stringify({username:username, password: password}));
     };
      
           
