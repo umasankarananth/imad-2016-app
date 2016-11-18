c915c5fb2511a65e7c419b4ed1bddca1e2d6545d
umasankarananth <parumasan12@gmail.com>
2016-11-18 14:58:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
-//var session =  require('express-session');
+var session =  require('express-session');
 
 var config = {
       user: 'umasankarananth',
@@ -17,10 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-//app.use(session({
-  //  secret: 'someRandomSecretValue',
-    //cookie: { maxAge: 1000 * 60 *60 *24 * 30}
-//}));
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 *60 *24 * 30}
+}));
 
 function create(dt){
     var fruitName =  dt.fruitName;
@@ -126,7 +126,7 @@ app.post('/login',function(req,res){
               if(hashedPassword === dbString){
                   
                   //set the session
-                 // req.session.auth = {userId: result.rows[0].id};
+                   req.session.auth = {userId: result.rows[0].id};
                   //set cookie with a session id
                   //Internally on the sever side, it maps the session id to an object
                   //{auth: {userId}}
@@ -142,13 +142,13 @@ app.post('/login',function(req,res){
   });
 });
 
-//app.get('/check-login', function(req,res){
-  //  if(req.session && req.session.auth && req.session.auth.userId){
-    //    res.send('You are Logged in:', +req.session.auth.userId.toString());
-//    }else {
-  //      res.send('you are not logged in');
-//    }
-//});
+app.get('/check-login', function(req,res){
+    if(req.session && req.session.auth && req.session.auth.userId){
+        res.send('You are Logged in:', +req.session.auth.userId.toString());
+    }else {
+        res.send('you are not logged in');
+    }
+});
 
 //app.get('/logout', function(req,res){
   // delete req.session.auth;
