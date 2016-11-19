dea1049942587f94331b10f8fa112b1db156c81f
umasankarananth <parumasan12@gmail.com>
2016-11-19 13:37:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,38 +1,84 @@
 
   //SUBMIT username password to Login
   
-  var sub = document.getElementById('sub_btn');
+ function loadLoginForm () {
+    var loginHtml = `
+        <h3>Login/Register to unlock awesome features</h3>
+        <input type="text" id="username" placeholder="username" />
+        <input type="password" id="password" />
+        <br/><br/>
+        <input type="submit" id="login_btn" value="Login" />
+        <input type="submit" id="register_btn" value="Register" />
+        `;
+    document.getElementById('login_area').innerHTML = loginHtml;
+    
+    // Submit username/password to login
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
   
-  sub.onclick = function(){ 
-           //Create a request Object
-                var request = new XMLHttpRequest();
-           // capture the response & stored in a variable
-                 request.onreadystatechange = function(){
-                 if(request.readyState === XMLHttpRequest.DONE){
-                     //Take some action
-                     if(request.status === 200){
-                         console.log('user logged in');
-                         alert('Logged in Successfully');
-                     }
-                     else if(request.status=== 403){
-                         alert('Username/Password is Incorrect');
-                     }
-                     else if(request.status===500){
-                         alert('Something went wrong on the server');
-                     }
-                    
-                }
-                //Not done yet
-         };
-          //Make request to server and send the name
-                     var username = document.getElementById('username').value;
-                      var password = document.getElementById('password').value;
-                      console.log(username);
-                      console.log(password);
-                     request.open('POST','http://umasankarananth.imad.hasura-app.io/login', true);
-                     request.setRequestHeader('Content-Type','application/json');
-                     request.send(JSON.stringify({username:username, password: password}));
-                          
+   var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+}
 
-  };
-          
+      
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
