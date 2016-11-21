5a6c8ee10f246ece9553267e27b02c4f6fb70825
umasankarananth <parumasan12@gmail.com>
2016-11-21 23:48:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -153,5 +153,23 @@ var commentobj =`
     
 }
 
+function loadLoginNext () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                commentFormHtml(this.responseText);
+            } 
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
+
+
+
+
 loadLogin();
-commentFormHtml();
+loadLoginNext();
