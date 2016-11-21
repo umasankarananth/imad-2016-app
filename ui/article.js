5eab9d6f3e0a9a4251ea274b30e53d8bfefeba23
umasankarananth <parumasan12@gmail.com>
2016-11-21 09:38:24 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,8 +1,1 @@
-    var commentFormHtml = `
-        <h5>Submit a comment</h5>
-        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-        <br/>
-        <input type="submit" id="submit" value="Submit" />
-        <br/>
-        `;
-    document.getElementById('comment_form').innerHTML = commentFormHtml;
+   
