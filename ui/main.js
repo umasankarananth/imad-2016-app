39a6f665335a8d9b427c40b19ffaa8bf1e9ab133
umasankarananth <parumasan12@gmail.com>
2016-11-22 20:14:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -108,7 +108,18 @@ function loadLogin () {
     request.send(null);
 }
 
-
+function loadCommentForm () {
+    var commentFormHtml = `
+        <h5>Submit a comment</h5>
+        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+        <br/>
+        <input type="submit" id="submit" value="Submit" />
+        <br/>
+        `;
+    document.getElementById('comment_form').innerHTML = commentFormHtml;
+    
+    
+}
 
 function loadLoginNext() {
     // Check if the user is already logged in
@@ -129,3 +140,4 @@ function loadLoginNext() {
 
 loadLogin();
 loadLoginNext();
+loadCommentForm();
