f4eda0f4ac0264ebdb869ecf82128862165cf7b6
umasankarananth <parumasan12@gmail.com>
2016-11-22 21:08:19 +0530

    [imad-console] Updates ui/fruitJS.js

--- ui/fruitJS.js
+++ ui/fruitJS.js
@@ -1,3 +1,19 @@
+function loadCommentForm () {
+    var commentFormHtml = `
+        <h5>Submit a comment</h5>
+        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+        <br/>
+        <input type="submit" id="submit" value="Submit" />
+        <br/>
+        `;
+        var obj = document.getElementById('commentform');
+       obj.innerHTML = commentFormHtml;
+   
+    
+}
+
+
+
 function loadLoginNext() {
     // Check if the user is already logged in
     var request = new XMLHttpRequest();
@@ -13,5 +29,6 @@ function loadLoginNext() {
     request.open('GET', '/check-login', true);
     request.send(null);
 }
+
 loadLoginNext();
 //loadCommentForm();
