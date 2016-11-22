2edc8d669b59d876521615022a604970a4ee6ab6
umasankarananth <parumasan12@gmail.com>
2016-11-22 21:23:01 +0530

    [imad-console] Updates ui/fruitJS.js

--- ui/fruitJS.js
+++ ui/fruitJS.js
@@ -1,13 +1,8 @@
 function loadCommentForm () {
-    var commentFormHtml = `
-        <h5>Submit a comment</h5>
-        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-        <br/>
-        <input type="submit" id="submit" value="Submit" />
-        <br/>
-        `;
-        var obj = document.getElementById('commentform');
-       obj.innerHTML = commentFormHtml;
+   
+        var obj = document.getElementById('comment_text').value;
+        var submit =document.getElementById('submit'); 
+      
    
     
 }
