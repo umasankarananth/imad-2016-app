6614dd136a42492fab05e40db4e7d1b32c90a072
umasankarananth <parumasan12@gmail.com>
2016-11-22 23:50:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -108,6 +108,41 @@ function loadLogin () {
     request.send(null);
 }
 
+//Submit username/password to login
+var currentArticleTitle = window.location.pathname.split('/')[2];
+
+function loadCommentForm () {
+        window.alert('fruitJs  is called');
+       var submit = document.getElementById('submit');
+       submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+                // Take some action
+                if (request.status === 200) {
+                    // clear the form & reload all the comments
+                    document.getElementById('comment_text').value = " ";
+                    //loadComments();    
+                } else {
+                    alert('Error! Could not submit comment');
+                }
+                submit.value = 'Submit';
+          }
+        };
+
+  
+// Make the request
+        var comment = document.getElementById('comment_text').value;
+        request.open('POST', '/submit-comment/' + currentArticleTitle, true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({comment: comment}));  
+        submit.value = 'Submitting...';
+        
+    };
+}
 
 function loadLoginNext() {
     // Check if the user is already logged in
@@ -116,7 +151,7 @@ function loadLoginNext() {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
                 window.alert('loginnext is loaded');
-               // loadCommentForm();
+                loadCommentForm();
                  
             }
         }
