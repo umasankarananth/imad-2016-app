a1a01dca51ab3b996694208be541d9681f692270
umasankarananth <parumasan12@gmail.com>
2016-11-23 18:20:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -223,6 +223,9 @@ app.post('/submit-comment/:articleName', function (req, res) {
     }
 });
 
+app.get('/ui/fruitJS.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'fruitJS.js'));
+});
 app.get('/welcomeform', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'welcomeform.html'));
 });
