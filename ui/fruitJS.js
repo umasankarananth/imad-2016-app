59a5460811ff7d47df783fee25b93567bfc1df8c
umasankarananth <parumasan12@gmail.com>
2016-11-23 22:54:22 +0530

    [imad-console] Updates ui/fruitJS.js

--- ui/fruitJS.js
+++ ui/fruitJS.js
@@ -37,6 +37,7 @@ function loadCommentForm () {
 // Make the request
         var comment = document.getElementById('comment_text').value;
         window.alert('submitcomment is going to call');
+        window.alert(comment);
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({comment: comment}));  
