c6ef053d50170ed096723f2820ca29899631f413
umasankarananth <parumasan12@gmail.com>
2016-11-24 17:48:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -219,12 +219,12 @@ app.post('/submit-comment/:articleName', function (req, res) {
                 if (result.rows.length === 0) {
                     res.status(400).send('Article not found');
                 } else {
-                    var articId = result.rows[0].id;
+                    var articleId = result.rows[0].id;
                     console.log(articId);
                     console.log(req.body.comment);
                     // Now insert the right comment for this article
                     pool.query("INSERT INTO comment (articleId, usrId,comment) VALUES ($1, $2, $3)",
-                               [articId, req.session.auth.userId, req.body.comment],function (err, result) {
+                               [articleId, req.session.auth.userId, req.body.comment],function (err, result) {
                             console.log('Insert query executed');       
                             if (err) {
                                 res.status(500).send(err.toString());
