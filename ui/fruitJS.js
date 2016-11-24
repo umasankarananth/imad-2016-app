465e0790238553f658371188cddd03c61f52f8ed
umasankarananth <parumasan12@gmail.com>
2016-11-24 23:25:48 +0530

    [imad-console] Updates ui/fruitJS.js

--- ui/fruitJS.js
+++ ui/fruitJS.js
@@ -12,43 +12,9 @@ function loadCommentForm () {
         `;
        document.getElementById('comment_form').innerHTML = commentFormHtml;
 
-       var submit = document.getElementById('submit');
-       submit.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-                // Take some action
-                if (request.status === 200) {
-                    // clear the form & reload all the comments
-                    document.getElementById('comment_text').value =" ";
-                   // loadComments();    
-                } else {
-                    alert('Error! Could not submit comment');
-                }
-                submit.value = 'Submit';
-          }
-        };
-
-  
-// Make the request
-        var comment = document.getElementById('comment_text').value;
-        window.alert('submitcomment is going to call');
-        
-        request.open('POST', '/submit-comment/' + currentArticleTitle, true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({comment: comment}));  
-        window.alert(comment);
-        submit.value = 'Submitting...';
-        
-    };
+       
 }
-        
-
-
-
+       
 function loadLogin() {
     // Check if the user is already logged in
     var request = new XMLHttpRequest();
