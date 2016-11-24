ba30944f64f82283b52268aed376f3648ea9ac43
umasankarananth <parumasan12@gmail.com>
2016-11-24 14:16:13 +0530

    [imad-console] Updates ui/fruitJS.js

--- ui/fruitJS.js
+++ ui/fruitJS.js
@@ -24,7 +24,7 @@ function loadCommentForm () {
                 // Take some action
                 if (request.status === 200) {
                     // clear the form & reload all the comments
-                    document.getElementById('comment_text').value ="";
+                    document.getElementById('comment_text').value =" ";
                     loadComments();    
                 } else {
                     alert('Error! Could not submit comment');
