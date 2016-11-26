1d4498959db79f9191a0c9fc1fb6db5b11cea11a
umasankarananth <parumasan12@gmail.com>
2016-11-26 12:14:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -193,7 +193,7 @@ app.get('/get-articles', function (req, res) {
    // return a response with the results
    // pool.query('SELECT comment.*, "user".username FROM fruitprice, comment, "user" WHERE fruitprice.fruitname = $1 AND fruitprice.id = //comment.articleId AND comment.usrId = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result//){
    app.get('/get-comments',function(req,res){
-   pool.query("SELECT * FROM comment,fruitprice where comment.articleId = '"+ fruitprice.id+"'",function(err,result){
+   pool.query("SELECT fruitprice.*, comment.* FROM fruitprice,comment where comment.articleId = '"+ fruitprice.id+"'",function(err,result){
       console.log(result);
       if (err) {
           res.status(500).send(err.toString());
