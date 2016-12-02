1cf0d2c9a3578082b4dd929a69d25ad207d159c2
umasankarananth <parumasan12@gmail.com>
2016-12-02 23:07:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -233,9 +233,10 @@ app.post('/submit-comment/:articleName', function (req, res) {
 });
 
 
-
+var counter = 0;
 app.get('/fruits/:frname', function(req,res){
    var frname = req.params.frname;
+   counter = counter + 1;
    pool.query('SELECT * FROM fruitprice where fruitname =$1', [req.params.frname], function(err,result){
       if (err){
           res.status(500).send(err.toString());
@@ -246,6 +247,7 @@ app.get('/fruits/:frname', function(req,res){
                
                var frutData =result.rows[0];
                res.send(create(frutData));
+               res.send(counter.toString());
                 } 
      
           }
